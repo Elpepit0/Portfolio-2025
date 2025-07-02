4ceb1e189e4d400aae4ea142fd6a926e269950ff
Yohan Boiroux <yohanboiroux@gmail.com>
2025-07-02 17:51:20 +0200

    reset deployement

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1000, // Augmente la limite de taille des chunks
-  },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 8080,
